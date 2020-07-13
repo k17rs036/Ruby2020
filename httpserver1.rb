2fde6914ee46feec415766b9c2bfe3726bcd96fe
Kawamoto Aoi <44153020+k17rs036@users.noreply.github.com>
2020-07-13 10:40:44 +0900

    マルチスレッド化

--- httpserver1.rb
+++ httpserver1.rb
@@ -2,15 +2,33 @@
 
 require 'socket'
 
-s=TCPServer.open 80
+def server sock
+    body=""
+    while line=sock.gets
+        line.chomp!
+        break if line==""
+        p line
+        body += "receive: #{line}<br>"
+    end
 
-sock=s.accept
-while line=sock.gets
-    line.chomp!
-    break if line==""
-    p line
-    sock.puts "receive: #{line}"
+    sock.puts "HTTP/1.0 200 OK"
+    sock.puts ""
+    sock.puts "<!DOCTYPE html>"
+    sock.puts "<html><body>"
+    sock.puts "<h1>my server</h1>"
+    sock.puts body
+    sock.puts "</html></body>"
+    sock.close
 end
-sock.close
+
+TCPServer.open 80 do |s|
+
+#マルチスレッド化
+    while true
+        Thread.new s.accept do |sock|
+            server sock
+        end
+    end
+end
 
 s.close
