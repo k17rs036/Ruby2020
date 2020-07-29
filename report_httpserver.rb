7f609c6551307bd9b9324f1cca1474f0575a6ee5
Kawamoto Aoi <44153020+k17rs036@users.noreply.github.com>
2020-07-30 02:09:59 +0900

    現在の天気を教えてくれる機能の追加

--- report_httpserver.rb
+++ report_httpserver.rb
@@ -1,9 +1,21 @@
-# httpserver1.rb
+# report_httpserver.rb
 
 require 'socket'
 require 'date'
+require "json"
+require "open-uri"
+require "./key"
+
+API_KEY = $api_key
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather/"
+ZIP_COAD = "813-0004"
 
 def server sock
+    response = open(BASE_URL + "?&zip=#{ZIP_COAD},JP&units=metric&appid=#{API_KEY}")
+    data = JSON.parse(response.read, {symbolize_names: true})
+    weather = data[:weather][0][:main]
+    t1 = "現在の福岡市の天気は"
+    t2 = "です。"
     body=""
     #l1=""
     while line=sock.gets
@@ -15,18 +27,27 @@ def server sock
             path=$1
             if path=="/fn"
                 body += "-Functions List-"+
-                "\n\"now\""+"               "+"Answer the Current time"+
-                "\n\"hello\""+"             "+"Return HELLO!"+
-                "\n\"current-weather\""+"   "+"Answer the current weather in Fukuoka"+
-                "\n\"today-weather\""+"     "+"Answer the today weather in Fukuoka"+ 
-                "\n\"tomorrow-weather\""+"  "+"Answer the tomorrow weather in Fukuoka"
+                "\n\"hello\""+"    "+"Return HELLO!"+
+                "\n\"now\""+"      "+"Answer the Current time"+
+                "\n\"cw\""+"       "+"Answer the Current weather in Fukuoka"+
+                "\n\"tdw\""+"      "+"Answer the Today's weather in Fukuoka"+ 
+                "\n\"tmw\""+"      "+"Answer the Tomorrow's weather in Fukuoka"
             elsif path=="/hello"
                 body += "HELLO!"
             elsif path=="/now"
                 body += DateTime.now.to_s
-            # elsif path==""
-            #     body += "Functions
-            #     hello"
+            elsif path=="/cw"
+                if weather == "Clear"
+                    body += t1+"\"晴れ\""+t2
+                elsif weather == "Clouds"
+                    body += t1+"\"くもり\""+t2
+                elsif weather == "Rain"
+                    body += t1+"\"雨\""+t2
+                elsif weather == "Snow"
+                    body += t1+"\"雪\""+t2
+                else
+                    body weather
+                end
             else
                 body += "unknown #{path}"
             end
